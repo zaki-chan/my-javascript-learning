992083c70aff0c6305f0ce191d74d4f9e4c77b9a
zaki-chan <naotti0926@gmail.com>
2025-09-09 21:42:17 +0900

    start-using-react

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -1,36 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// const subject = "React";
+function App(props){
   return (
-    // フラグメント：Reactコンポーネントは単一のJSX要素を返す。
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        {/* 中括弧の中にはjavascript式を入れることができる。 */}
+        <h1>Hello, {props.subject} !</h1>
+        <button type="button" className="primary">Click me!</button>
+      </header>
     </>
-  )
+  );
 }
-// export文は他からApp関数を利用できるようにする。
-export default App
+
+export default App;
