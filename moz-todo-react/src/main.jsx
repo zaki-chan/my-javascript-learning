306cdeba39a05dd042ecebd8211ca82b9f57fa97
zaki-chan <naotti0926@gmail.com>
2025-09-09 23:32:42 +0900

    learning-to-make-component
    define components of Form and FilterButton. use objects array as props data. use map to assign data to component's props

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const DATA = [
+  {id:"todo-0",name:"Eat",completed:true},
+  {id:"todo-1",name:"Sleep",completed:false},
+  {id:"todo-2",name:"Repeat",completed:false}
+];
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App subject="Clarice"/>
+    <App tasks={DATA}/>
   </StrictMode>,
 )
