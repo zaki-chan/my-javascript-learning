7107b43cf00d87b729126ef61df62f2042210c69
zaki-chan <naotti0926@gmail.com>
2025-09-19 18:13:42 +0900

    finish dev todo app

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import Todo from "./components/Todo.jsx";
 import Form from "./components/Form.jsx";
 import FilterButton from "./components/FilterButton.jsx";
 
+function usePrevious(value){
+  const ref = useRef();
+  useEffect(()=> {
+    ref.current = value;
+  });
+  return ref.current;
+}
+// App()関数外での定義は、<App />コンポーネントが際レンダリングされても
+// 再計算されない
+const FILTER_MAP = {
+  All:() =>true,
+  Active: (task) => !task.completed,
+  Completed: (task) =>task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props){
   const [tasks,setTasks] = useState(props.tasks);
+  const [filter,setFilter] = useState("All");
+
+  const filterList = FILTER_NAMES.map((name)=>(
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name===filter}
+      setFilter={setFilter}
+    />
+  ))
 
   function toggleTaskCompleted(id){
     const updatedTasks = tasks.map((task) =>{
@@ -18,12 +44,22 @@ function App(props){
     setTasks(updatedTasks);
   }
 
+  function editTask(id,newName){
+    const editedTaskList = tasks.map((task) =>{
+      if(id===task.id){
+        return {...task,name:newName};
+      }
+
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
   function deleteTask(id){
     const remainingTasks = tasks.filter((task)=>id!== task.id);
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks?.map((task)=>(
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task)=>(
     <Todo
       key={task.id}
       id={task.id}
@@ -31,6 +67,7 @@ function App(props){
       completed={task.completed}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
@@ -42,17 +79,26 @@ function App(props){
   const tasksNoun = taskList.length !==1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
+  const listHeadingRef = useRef(null);
+
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(()=>{
+    if(tasks.length < prevTaskLength){
+      listHeadingRef.current.focus();
+    }
+  },[tasks.length,prevTaskLength]);
   return(
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
 
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
 
       <ul
         role="list"
--- moz-todo-react/src/components/Todo.jsx
+++ moz-todo-react/src/components/Todo.jsx
@@ -1,22 +1,81 @@
+import { useEffect, useRef, useState } from "react";
+
+function usePrevious(value){
+    const ref = useRef();
+    useEffect(()=>{
+        ref.current = value;
+    });
+    return ref.current;
+}
+
 function Todo(props){
-    return(
-        <li className="todo stack-small">
+    const [isEditing,setEditing] = useState(false);
+    const [newName,setNewName] = useState("");
+
+    const editFieldRef = useRef(null);
+    const editButtonRef = useRef(null);
+
+    const wasEditing = usePrevious(isEditing);
+    console.log(wasEditing);
+
+    function handleChange(e){
+        setNewName(e.target.value);
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        props.editTask(props.id,newName);
+        setNewName("");
+        setEditing(false);
+    }
+
+    const editingTemplate = (
+        <form className="stack-small" onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label className="todo-label" htmlFor={props.id}>
+                    New name for {props.name}
+                </label>
+                <input
+                    id={props.id}
+                    className="todo-text"
+                    type="text"
+                    value={newName}
+                    onChange={handleChange}
+                    ref={editFieldRef}
+                />
+            </div>
+            <div className="btn-group">
+                <button type="button" className="btn todo-cancel" onClick={()=>setEditing(false)}>
+                    Cancel
+                    <span className="visually-hidden">renaming {props.name}</span>
+                </button>
+                <button type="button" className="btn btn__primary todo-edit">
+                    Save
+                    <span className="visually-hidden">new name for {props.name}</span>
+                </button>
+            </div>
+        </form>
+    );
+
+    const viewTemplate = (
+        <div className="stack-small">
             <div className="c-cb">
                 <input
                     id={props.id}
                     type="checkbox"
-                    defaultChecked = {props.completed}
-                    onChange={() =>
-                        props.toggleTaskCompleted(props.id)
-                    }
+                    defaultChecked={props.completed}
+                    onChange={() => props.toggleTaskCompleted(props.id)}
                 />
-
-                <label className="todo-label" htmlFor="todo-0">
+                <label className="todo-label" htmlFor={props.id}>
                     {props.name}
                 </label>
             </div>
             <div className="btn-group">
-                <button type="button" className="btn">
+                <button
+                    type="button"
+                    className="btn"
+                    onClick={() => setEditing(true)}
+                    ref={editButtonRef}>
                     Edit <span className="visually-hidden">{props.name}</span>
                 </button>
                 <button
@@ -26,8 +85,20 @@ function Todo(props){
                     Delete <span className="visually-hidden">{props.name}</span>
                 </button>
             </div>
-        </li>
+        </div>
     );
+
+    // useEffectはコンポーネントのレンダリング後に実行される
+    useEffect(()=>{
+        if(!wasEditing && isEditing){
+            editFieldRef.current.focus();
+        }else if(wasEditing && !isEditing){
+            editButtonRef.current.focus();
+        }
+    },[wasEditing,isEditing]);
+    console.log("main render");
+
+    return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
 
 export default Todo;
